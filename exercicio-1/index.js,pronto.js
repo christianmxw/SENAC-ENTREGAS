0beba1e793507d100f8bbdaf7af324389f5089ea
chris.mxw <166619985+christianmxw@users.noreply.github.com>
2024-04-16 11:32:34 -0300

    exercício 1 pronto.

---
+++ exercicio-1/index.js,pronto.js
@@ -0,0 +1,9 @@
+let nome
+let idade
+console.log("Tipo de nome:", typeof nome)
+console.log("Tipo de idade:",typeof idade)
+//nome e idade indefinos//
+nome = prompt ("Qual seu nome?")
+idade = prompt ("Qual a sua idade?")
+//novas variaveis para perguntar o nome e idade//
+console.log("ola", nome, "voce tem", idade, "anos")
