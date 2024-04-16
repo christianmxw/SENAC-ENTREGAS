3a2643515781e6669f76d72b3af96a926961753c
chris.mxw <166619985+christianmxw@users.noreply.github.com>
2024-04-16 11:48:50 -0300

    Delete exercicio-1 directory

--- exercicio-1/index.js,pronto.js
+++
@@ -1,9 +0,0 @@
-let nome
-let idade
-console.log("Tipo de nome:", typeof nome)
-console.log("Tipo de idade:",typeof idade)
-//nome e idade indefinos//
-nome = prompt ("Qual seu nome?")
-idade = prompt ("Qual a sua idade?")
-//novas variaveis para perguntar o nome e idade//
-console.log("ola", nome, "voce tem", idade, "anos")
